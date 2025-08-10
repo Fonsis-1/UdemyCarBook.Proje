ac476b9827d3b382cd8f7cc7c0958e7f574f610a
Yusuf Esüntimur <yusufesuntimur@gmail.com>
2025-08-11 00:39:01 +0300

    Add project files.

---
+++ Frontends/UdemyCarBook.WebUI/Controllers/CarController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarBook.Dto.CarDtos;
+
+namespace UdemyCarBook.WebUI.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CarController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var reponseneMessage = await client.GetAsync("https://localhost:7173/api/Cars/GetCarWithBrand");
+            if (reponseneMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await reponseneMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandsDtos>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ Frontends/UdemyCarBook.WebUI/Controllers/ServiceController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarBook.Dto.ServiceDtos;
+
+namespace UdemyCarBook.WebUI.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ServiceController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var reponseneMessage = await client.GetAsync("https://localhost:7173/api/Services");
+            if (reponseneMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await reponseneMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
